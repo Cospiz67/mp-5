8824dea98db10733ac4e46540721f57f18a60542
Cospiz67 <jeanne.durand@epita.fr>
2026-04-01 23:46:24 -0400

    start project create db elmt + check alias

---
+++ lib/checkValidityAlias.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import getCollection, {URL_COLLECTION} from "@/db";
+
+
+export default async function checkValidityAlias(alias:string){
+
+  const urlCollection = await getCollection(URL_COLLECTION);
+  const data=await urlCollection.findOne({alias:alias});
+
+    if(data===null){
+        return null;
+    }
+
+    return(true);
+}
---
+++ lib/createNewURL.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import getCollection, {URL_COLLECTION} from "@/db";
+
+
+export default async function createNewURL(url:string, alias:string){
+  const p ={
+    url: url,
+    alias: alias,
+  }
+
+  const urlCollection = await getCollection(URL_COLLECTION);
+  const resDB = await urlCollection.insertOne(p);
+}
