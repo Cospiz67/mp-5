499e5989712ba10977feb4a5d943457007f10b6c
Cospiz67 <jeanne.durand@epita.fr>
2026-04-06 15:22:26 -0400

    error page with notFound

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,11 +1,11 @@
 "use server";
 import checkValidityAlias from "@/lib/checkValidityAlias";
-import {redirect} from "next/navigation";
+import {notFound, redirect} from "next/navigation";
 
 export default async function urlRedirect({params}:{params:Promise<{alias:string}>}){
     const res = await checkValidityAlias((await params).alias);
     console.log(res);
     if(res === null)
-        redirect(`/error`);
+        notFound
     redirect(res);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>URL Shortener</title>
       <body>
         <StyledDiv>
           <Header/>
